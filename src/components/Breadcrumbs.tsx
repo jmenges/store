3c84fc130a5d141ca562290c7e25e49d5a8ff15c
Jonas Menges <dev.menges.jonas@gmail.com>
2023-08-01 09:50:32 +0200

    Update breadcrumb styling

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -10,12 +10,12 @@ type Props = { title: string; items: Breadcrumb[] };
 
 export default function Breadcrumbs({ title, items }: Props) {
   return (
-    <div className="mb-8 bg-gray-200/60 py-8">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-semibold">{title}</h1>
-        <div className="inline-flex gap-2 font-mono text-sm font-light uppercase tracking-wider text-gray-800 [&>a:not(:last-child)]:after:content-['/'] [&>a:not(:last-child)]:after:ml-2">
+    <div className="mb-8 maxbg-gray-200/60 py-4 sm:py-8">
+      <div className="container mx-auto max-sm:flex max-sm:items-end max-sm:gap-2">
+        <h1 className="text-xl sm:text-2xl font-semibold max-sm:leading-none max-sm:hidden">{title}</h1>
+        <div className="inline-flex gap-2 font-mono text-xs sm:text-sm font-light uppercase tracking-wider text-gray-800 [&>a:not(:last-child)]:after:content-['/'] [&>a:not(:last-child)]:after:ml-2">
           {items.map((breadcrumb) => (
-            <Link key={breadcrumb.title} href={breadcrumb.href}>
+            <Link className="" key={breadcrumb.title} href={breadcrumb.href}>
               {breadcrumb.title}
             </Link>
           ))}
