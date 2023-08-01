817263570577e0c2e4eb1f7e058444f5750a952e
Jonas Menges <dev.menges.jonas@gmail.com>
2023-08-01 09:53:43 +0200

    Fix typo in classname

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -10,7 +10,7 @@ type Props = { title: string; items: Breadcrumb[] };
 
 export default function Breadcrumbs({ title, items }: Props) {
   return (
-    <div className="mb-8 maxbg-gray-200/60 py-4 sm:py-8">
+    <div className="mb-8 bg-gray-200/60 py-4 sm:py-8">
       <div className="container mx-auto max-sm:flex max-sm:items-end max-sm:gap-2">
         <h1 className="text-xl sm:text-2xl font-semibold max-sm:leading-none max-sm:hidden">{title}</h1>
         <div className="inline-flex gap-2 font-mono text-xs sm:text-sm font-light uppercase tracking-wider text-gray-800 [&>a:not(:last-child)]:after:content-['/'] [&>a:not(:last-child)]:after:ml-2">
