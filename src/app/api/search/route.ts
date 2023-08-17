27cdfce7c93af0e40204732cbcaac2cfb0da41bb
Jonas Menges <dev.menges.jonas@gmail.com>
2023-08-17 16:10:03 +0200

    remove packages for testing

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,16 +1,16 @@
 import { getProducts } from "@/lib/shopify/operations/product";
 import { NextResponse } from "next/server";
 
-import { z } from "zod";
+// import { z } from "zod";
 
-const termSchema = z.string().regex(/^[A-Za-z0-9\s]*$/);
+// const termSchema = z.string().regex(/^[A-Za-z0-9\s]*$/);
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   try {
     const term = searchParams.get("term");
-    const termParsed = termSchema.parse(term);
-    const searchQuery = `title:${termParsed}*`;
+    // const termParsed = termSchema.parse(term);
+    const searchQuery = `title:${term}*`;
     const products = await getProducts({
       query: searchQuery,
       first: 6,
